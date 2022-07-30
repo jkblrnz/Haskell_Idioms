9fd35492a9680344e1930a810d76fad34efef4ab
Jakob <jkblrnz@gmail.com>
2022-07-30 14:51:37 -0500

    Simplified mergesort without the possibly superfluous optomization

--- Foldt.hs
+++ Foldt.hs
@@ -10,17 +10,25 @@ foldt _ z []  = z
 foldt f z [x] = f x z
 foldt f z xs  = foldt f z (pairs f xs)
 
-mergeBy :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
-mergeBy cmp = merge where
-  merge [] ys = ys
-  merge xs [] = xs
-  merge (x:xs) (y:ys)
-    = case cmp x y of
-      GT -> y : merge (x:xs) ys
-      _  -> x : merge xs (y:ys)
+-- mergeBy :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+-- mergeBy cmp = merge where
+--   merge [] ys = ys
+--   merge xs [] = xs
+--   merge (x:xs) (y:ys)
+--     = case cmp x y of
+--       GT -> y : merge (x:xs) ys
+--       _  -> x : merge xs (y:ys)
 
-sortBy :: (a -> a -> Ordering) -> [a] -> [a]
-sortBy cmp = foldt (mergeBy cmp) [] . map (: [])
+-- sortBy :: (a -> a -> Ordering) -> [a] -> [a]
+-- sortBy cmp = foldt (mergeBy cmp) [] . map (: [])
 
-sort :: Ord a => [a] -> [a]
-sort = sortBy compare
+-- sort :: Ord a => [a] -> [a]
+-- sort = sortBy compare
+
+merge [] ys = ys
+merge xs [] = xs
+merge xs@(x:xt) ys@(y:yt)
+  | x <= y = x : merge xt ys
+  | otherwise = y: merge xs yt
+
+mergesort xs = foldt merge [] [[x] | x <- xs]
