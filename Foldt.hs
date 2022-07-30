6abfb1dcd8031363b1033ecfae3b7856ca69a018
Jakob <jkblrnz@gmail.com>
2022-07-30 14:03:06 -0500

    foldt demo via Ian Lynagh's mergesort from Data.List.Sort

---
+++ Foldt.hs
@@ -0,0 +1,26 @@
+-- mergesort to demonstrate expressiveness of foldt
+module Foldt where
+
+pairs :: (a -> a -> a) -> [a] -> [a]
+pairs f (x:y:z) = f x y : pairs f z
+pairs _ z       = z
+
+foldt :: (a -> a -> a) -> a -> [a] -> a
+foldt _ z []  = z
+foldt f z [x] = f x z
+foldt f z xs  = foldt f z (pairs f xs)
+
+mergeBy :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+mergeBy cmp = merge where
+  merge [] ys = ys
+  merge xs [] = xs
+  merge (x:xs) (y:ys)
+    = case cmp x y of
+      GT -> y : merge (x:xs) ys
+      _  -> x : merge xs (y:ys)
+
+sortBy :: (a -> a -> Ordering) -> [a] -> [a]
+sortBy cmp = foldt (mergeBy cmp) [] . map (: [])
+
+sort :: Ord a => [a] -> [a]
+sort = sortBy compare
